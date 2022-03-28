511ab69600d5daead99dc245dc8242feded5b277
Thomas Paul Betterly <thomasbetterly@aol.com>
2022-03-28 17:24:03 -0400

    Working towards renaming Filesandbox to insight

--- FileSandBox_GUI/Properties/Resources.Designer.cs
+++ FileSandBox_GUI/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace InsightSheath.Properties {
+namespace InsightDebugger_GUI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace InsightSheath.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FileSandBox_GUI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("InsightDebugger_GUI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
